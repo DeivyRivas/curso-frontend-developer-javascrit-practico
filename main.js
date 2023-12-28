0c0ecf0bc2570a15d8ecb5615024ce5b740d64ba
DeivyRivas <deyruiz555@gmail.com>
2023-12-28 11:23:58 -0500

    se asigna la opcion del detalle del prodcuto

--- main.js
+++ main.js
@@ -1,10 +1,56 @@
+//se crean la variable para seleccionar 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const LogoMenuMobile = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu');
 
+const logoCarrito = document.querySelector('.navbar-shopping-cart');
+const productdetail = document.querySelector('.product-detail')
+
+
+
+
+
+// se indica el evento a realizar recibe evento y luego la funcion
 menuEmail.addEventListener('click', toggleDesktopMenu)
 
+LogoMenuMobile.addEventListener('click', toggleMobileMenu)
 
+logoCarrito.addEventListener('click', toggleCarritoCompra)
+
+
+
+
+
+
+
+// funciones 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive')
+    const isCarritoCompraClosed = productdetail.classList.contains('inactive');
+    if(!isCarritoCompraClosed){
+     productdetail.classList.add('inactive');
+     } 
+
+    //desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobileMenu(){
+   const isCarritoCompraClosed = productdetail.classList.contains('inactive');
+   if(!isCarritoCompraClosed){
+    productdetail.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleCarritoCompra(){
+   const isMenuMobileClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMenuMobileClosed){
+        mobileMenu.classList.add('inactive');
+        }
+
+    productdetail.classList.toggle('inactive');
+
 }
